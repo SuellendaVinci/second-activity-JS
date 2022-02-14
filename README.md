a5a60ec6184d3760b865718ec9881c8763119676
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-02-14 18:51:40 -0300

    almost ready

---
+++ README.md
@@ -0,0 +1,26 @@
+<!-- Exercícios de Fixação 02
+
+Professor: Luis Bicalho
+Estudante: Suellen Camargo
+
+1 - Faça um programa que leia 2 números inteiros, calcule e exiba a média aritmética e o produto dos números. O seu programa deve, obrigatoriamente, utilizar as seguintes funções feitas por você:
+
+calcula_media(): recebe 2 números inteiros como parâmetros e retorne a média dos números; 
+calcula_produto(): recebe 2 números inteiros como parâmetros e retorna o produto dos números;
+
+2 - Faça um programa que calcule e imprima a média ponderada de um aluno. Inicialmente, o programa deverá ler o peso da primeira prova e o peso da segunda prova (ambos inteiros). A seguir, o programa deverá ler os seguintes dados do aluno: matrícula (inteiro), nota da primeira prova e nota da segunda prova (ambas reais). Por último, o programa deve mostrar a matricula do aluno seguida da sua média. O seu programa deve, obrigatoriamente, utilizar a seguinte função feita por você:
+
+calcula_media_ponderada(): recebe 2 notas (reais) e seus respectivos pesos (inteiros) como parâmetros e retorna a média ponderada dos valores;
+
+3 - Altere o programa do exercício 2 para que ele utilize a função {calcula_produto} do exercício 1 no cálculo da média ponderada;
+
+4 - Altere o programa do exercício 3 para que ele processe os dados de 2 alunos ao invés de apenas 1;
+
+5 - Faça um programa que leia um valor real representando o gasto realizado por um cliente de um restaurante e imprima o valor total a ser pago, considerando os 10% do garçom. O seu programa deve, obrigatoriamente, utilizar uma função que receba o valor do gasto e retorne o valor total a ser pago pelo cliente; 
+
+6 - Implemente, utilizando funções, um programa que receba do usuário o valor do salário bruto de um funcionário e imprima o valor com descontos de INSS, FGTS e Plano de Saúde. O programa deve utilizar uma função para calcular cada um dos descontos, sendo que estas funções devem receber o valor do salário bruto e retornar o valor a ser descontado. Os valores descontados devem ser:
+
+- INSS: 10% do valor bruto;
+- FGTS: 8% do valor bruto;
+- Plano de Saúde: R$100,00. -->
+
---
+++ script.js
@@ -0,0 +1,277 @@
+// EXERCISE 01
+
+const calcula_media = (x, y) => {
+  let average = (x + y) / 2;
+  return average;
+};
+
+const calcula_produto = (x, y) => {
+  let multiplication = x * y;
+  return multiplication;
+};
+
+let n1 = null;
+let n2 = null;
+
+let average = null;
+let multiplication = null;
+
+let first_number = document.getElementById("first_number");
+first_number.addEventListener("keyup", (e) => {
+  n1 = parseInt(e.target.value);
+});
+
+let second_number = document.getElementById("second_number");
+second_number.addEventListener("keyup", (e) => {
+  n2 = parseInt(e.target.value);
+});
+
+//keyup (aperta a tecla pressionando - keydown e quando sobe é keyup)
+
+let button_average = document.getElementById("calcula_media");
+button_average.addEventListener("click", () => {
+  average = calcula_media(n1, n2);
+  multiplication = calcula_produto(n1, n2);
+  let average_result = document.getElementById("average");
+  let multiplication_result = document.getElementById("multiplication");
+  average_result.textContent = `The average is: ${average}`;
+  multiplication_result.textContent = `The multiplication is: ${multiplication}`;
+});
+
+// EXERCISE 02
+
+const calcula_media_ponderada = (w1, w2, g1, g2) => {
+  let weighted_average = (w1 * g1 + w2 * g2) / (w1 + w2);
+  weighted_average = weighted_average.toFixed(1);
+  return weighted_average;
+};
+
+let register = null;
+let weight01 = null;
+let weight02 = null;
+let grade01 = null;
+let grade02 = null;
+
+let weightedAverage = null;
+
+let weight1 = document.getElementById("weight1");
+weight1.addEventListener("keyup", (e) => {
+  weight01 = parseInt(e.target.value);
+});
+
+let weight2 = document.getElementById("weight2");
+weight2.addEventListener("keyup", (e) => {
+  weight02 = parseInt(e.target.value);
+});
+
+let grade1 = document.getElementById("grade1");
+grade1.addEventListener("keyup", (e) => {
+  grade01 = parseFloat(e.target.value);
+});
+
+let grade2 = document.getElementById("grade2");
+grade2.addEventListener("keyup", (e) => {
+  grade02 = parseFloat(e.target.value);
+});
+
+let register01 = document.getElementById("register-student");
+register01.addEventListener("keyup", (e) => {
+    register = parseInt(e.target.value);
+})
+
+let button_waverage = document.getElementById("calcula_media_ponderada");
+button_waverage.addEventListener("click", () => {
+  weightedAverage = calcula_media_ponderada(
+    weight01,
+    weight02,
+    grade01,
+    grade02
+  );
+  let register_result = document.getElementById("register");
+  register_result.textContent = `The student register number is: ${register}`;
+  let weightedAverage_result = document.getElementById("weightedAverage");
+  weightedAverage_result.textContent = `The weighted average is: ${weightedAverage}`;
+});
+
+// EXERCISE 03
+
+const calcula_media_ponderada2 = (w1, w2, g1, g2) => {
+  let w_a = (calcula_produto(w1, g1) + calcula_produto(w2, g2)) / (w1 + w2);
+  w_a = w_a.toFixed(1);
+  return w_a;
+};
+
+let registerX = null;
+let w1 = null;
+let w2 = null;
+let g1 = null;
+let g2 = null;
+
+
+let weighted_average = null;
+
+let weight_1 = document.getElementById("weight01");
+weight_1.addEventListener("keyup", (e) => {
+  w1 = parseInt(e.target.value);
+});
+
+let weight_2 = document.getElementById("weight02");
+weight_2.addEventListener("keyup", (e) => {
+  w2 = parseInt(e.target.value);
+});
+
+let grade_1 = document.getElementById("grade01");
+grade_1.addEventListener("keyup", (e) => {
+  g1 = parseFloat(e.target.value);
+});
+
+let grade_2 = document.getElementById("grade02");
+grade_2.addEventListener("keyup", (e) => {
+  g2 = parseFloat(e.target.value);
+});
+
+let registerXX = document.getElementById("register-student01");
+registerXX.addEventListener("keyup", (e) => {
+    registerX = parseInt(e.target.value);
+})
+
+let buttonWaverage = document.getElementById("calcula_media_ponderada2");
+buttonWaverage.addEventListener("click", () => {
+  weighted_average = calcula_media_ponderada2(w1, w2, g1, g2);
+  let register_result01 = document.getElementById("register01");
+  register_result01.textContent = `The student register number is: ${registerX}`;
+  let weighted_average_result = document.getElementById("weighted-average");
+  weighted_average_result.textContent = `The weighted average is: ${weighted_average}`;
+});
+
+// EXERCISE 04
+
+const calcula_media_ponderada3 = (w1, w2, g1s1, g2s1, g1s2, g2s2) => {
+  let w_a =
+    (calcula_produto(w1, g1s1) +
+      calcula_produto(w1, g1s2) +
+      calcula_produto(w2, g2s1) +
+      calcula_produto(w2, g2s2)) /
+    (2 * (w1 + w2));
+  w_a = w_a.toFixed(1);
+  return w_a;
+};
+
+let a = null;
+let b = null;
+let g1s1 = null;
+let g2s1 = null;
+let g1s2 = null;
+let g2s2 = null;
+
+let wa = null;
+
+let weight001 = document.getElementById("weight001");
+weight001.addEventListener("keyup", (e) => {
+  a = parseInt(e.target.value);
+});
+
+let weight002 = document.getElementById("weight002");
+weight002.addEventListener("keyup", (e) => {
+  b = parseInt(e.target.value);
+});
+
+let grade001 = document.getElementById("grade001");
+grade001.addEventListener("keyup", (e) => {
+  g1s1 = parseFloat(e.target.value);
+});
+
+let grade002 = document.getElementById("grade002");
+grade002.addEventListener("keyup", (e) => {
+  g2s1 = parseFloat(e.target.value);
+});
+
+let grade003 = document.getElementById("grade003");
+grade003.addEventListener("keyup", (e) => {
+  g1s2 = parseFloat(e.target.value);
+});
+
+let grade004 = document.getElementById("grade004");
+grade004.addEventListener("keyup", (e) => {
+  g2s2 = parseFloat(e.target.value);
+});
+
+let button_wa = document.getElementById("calcula_wa");
+button_wa.addEventListener("click", () => {
+  wa = calcula_media_ponderada3(a, b, g1s1, g2s1, g1s2, g2s2);
+  let wa_result = document.getElementById("weighted-average01");
+  wa_result.textContent = `The weighted average of the class is: ${wa}`;
+});
+
+// EXERCISE 5
+
+const total_price = (price) => {
+  let total = 1.1 * price;
+  total = total.toFixed(2);
+  return total;
+};
+
+let price = null;
+
+let price01 = document.getElementById("price");
+price01.addEventListener("keyup", (e) => {
+  price = parseFloat(e.target.value);
+});
+
+let button_price = document.getElementById("show_price");
+button_price.addEventListener("click", () => {
+  price = total_price(price);
+  let price_result = document.getElementById("price_result");
+  price_result.textContent = `The final price is: ${price}`;
+});
+
+
+// EXERCISE 6
+
+const inss = (salary) => {
+  let inss_tax = 0.1 * salary;
+  return inss_tax;
+};
+
+const fgts = (salary) => {
+  let fgts_tax = 0.08 * salary;
+  return fgts_tax;
+};
+
+const healthcare_plan = (salary) => {
+  return 100;
+};
+
+const final_salary = (salary) => {
+  let final_s = salary - inss(salary) - fgts(salary) - healthcare_plan(salary);
+  return final_s;
+};
+
+const salaryInput = document.getElementById("salary");
+
+const calculateButton = document.getElementById("calculateButton");
+
+let salary = null;
+let inssValue = null;
+let fgtsValue = null;
+let healthCareValue = null;
+
+salaryInput.addEventListener("keyup", (e) => {
+  salary = e.target.value;
+});
+
+calculateButton.addEventListener("click", () => {
+  if (salary) {
+    inssValue = Number(inss(salary));
+    fgtsValue = Number(fgts(salary));
+    healthCareValue = Number(healthcare_plan(salary));
+
+    const inssResult = document.getElementById("inssResult");
+    const fgtsResult = document.getElementById("fgtsResult");
+    const healthCareReasult = document.getElementById("healthCareReasult");
+
+    inssResult.textContent = `INSS tax: R$ ${inssValue}`;
+    fgtsResult.textContent = `My FGTS: R$ ${fgtsValue}`;
+    healthCareReasult.textContent = `Health Care Value: R$ ${healthCareValue}`;
+  }
+}); 
